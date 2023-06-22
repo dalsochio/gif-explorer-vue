4fc3bd461ae3b6f57aab97bd6e7489227a84bc4d
Thiago Dalsochio <thiago@dalsochio.com>
2023-06-22 00:38:00 -0300

    ImagePage infos

--- src/pages/ImagePage.vue
+++ src/pages/ImagePage.vue
@@ -1,10 +1,17 @@
 <template>
-    <main v-if="image" class="grid grid-cols-12 w-full lg:h-full border rounded-md overflow-hidden">
-        <div class="col-span-8 bg-zinc-200">
-            <img :src="image.images.original.url" class="max-w-full max-h-full mx-auto" />
+    <main v-if="image"
+        class="grid grid-rows-2 lg:grid-rows-none lg:grid-cols-12 w-full lg:h-full border rounded-md overflow-hidden">
+        <div class="col-span-8 bg-zinc-200 flex">
+            <img :src="image.images.original.url" class="self-center max-w-full max-h-full mx-auto" />
         </div>
         <div class="col-span-4">
-            b
+            <div class="p-4">
+                <h1 class="text-2xl font-bold">{{ image.title }}</h1>
+                <p class="text-sm text-gray-500">{{ image.username }}</p>
+                <p class="text-sm text-gray-500">{{ image.import_datetime }}</p>
+                <p class="text-sm text-gray-500">{{ image.rating }}</p>
+            </div>
+            <!-- <p><img :src="image.user.avatar_url" /> {{ image.user.display_name }}</p> -->
         </div>
     </main>
 
@@ -30,6 +37,7 @@ watch(
     () => route.params.id,
     async (id) => {
         image.value = await fetchImageGif(id)
+        console.log(image)
     },
     { immediate: true }
 )
